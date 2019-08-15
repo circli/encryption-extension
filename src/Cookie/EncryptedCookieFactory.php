467701c2a73435fc9807ada8f44859043cc85756
Andreas Sundqvist <andreas@forme.se>
2019-08-16 00:39:04 +0200

    Implement cookie factory

--- src/Cookie/EncryptedCookieFactory.php
+++ src/Cookie/EncryptedCookieFactory.php
@@ -4,11 +4,24 @@ namespace Circli\Extensions\Encryption\Cookie;
 
 use Circli\WebCore\Contracts\CookieInterface;
 use Circli\WebCore\Contracts\CookieFactory;
+use ParagonIE\Halite\Cookie;
 
 final class EncryptedCookieFactory implements CookieFactory
 {
+    private $loadedCookies = [];
+    /** @var Cookie */
+    private $cookie;
+
+    public function __construct(Cookie $cookie)
+    {
+        $this->cookie = $cookie;
+    }
+
     public function getCookie(string $name): CookieInterface
     {
-        // TODO: Implement getCookie() method.
+        if (!isset($this->loadedCookies[$name])) {
+            $this->loadedCookies[$name] = new EncryptedCookie($name, $this->cookie);
+        }
+        return $this->loadedCookies[$name];
     }
 }
